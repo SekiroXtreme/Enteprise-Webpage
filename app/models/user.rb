29e88785d6af3dd4d93b28f4f2568300348d52b4
Deivid <JosedavidRH30@gmail.com>
2023-03-18 14:06:12 -0500

    add customer association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #has_many :customers
+  has_many :customers
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
