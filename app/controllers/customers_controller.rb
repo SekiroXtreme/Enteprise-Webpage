f95543f9244fdfc2bf98dc204898fed0897f8d32
Deivid <JosedavidRH30@gmail.com>
2023-03-21 18:37:36 -0500

    add crud methods

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
   def index
     @customers = Customer.all
   end
@@ -7,17 +9,41 @@ class CustomersController < ApplicationController
     @customer = Customer.new
   end
 
+  def show
+  end
+
   def create
     @customer = current_user.customers.build(customer_params)
 
     if @customer.save
-      redirect_to customers_path, notice: "Cliente creado papu"
+      redirect_to customers_path, notice: "New client created"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    debugger
+    if @customer.update(customer_params)
+      redirect_to customers_path, notice: "Customer was successfully updated." 
+    else
+      render :edit, status: :unprocessable_entity
+    end 
+  end
+
+
+  def destroy
+       @customer.destroy
+       redirect_to customers_path, notice: "Customer was successfully removed." 
+  end
   private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 
   def customer_params
     params.require(:customer).permit(:first_name, :last_name, :phone, :main_address, :status, :email)
